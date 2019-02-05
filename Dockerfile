573023c56c2353f8e474f96f13cecccf18214e29
Matt Nelson <matt.nelson@cerner.com>
2019-02-05 10:42:29 -0600

    Add JAVA_HOME environment variable

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,5 @@ RUN yum -y update \
  && amazon-linux-extras enable corretto8 \
  && yum -y install java-1.8.0-amazon-corretto-devel \
  && yum clean all
+
+ ENV JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64
--- test-image.sh
+++ test-image.sh
@@ -4,16 +4,26 @@ set -euo pipefail
 # 1. Fetch and setup test framework.
 # For more info and usage examples, see:
 #   https://github.com/GoogleContainerTools/container-structure-test
+
+# only linux and osx are supported
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  cst_ostype="linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  cst_ostype="darwin"
+else
+  echo "Unsupported operating system[$OSTYPE]"
+  exit 1
+fi
 mkdir -p build/tool
 wget -N \
      -P build/tool \
-      https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64
-chmod +x build/tool/container-structure-test-linux-amd64
+      https://storage.googleapis.com/container-structure-test/latest/container-structure-test-${cst_ostype}-amd64
+chmod +x build/tool/container-structure-test-${cst_ostype}-amd64
 
 # 2. Build the docker image.
 docker build --tag corretto-8 .
 
 # 3. Test using container-structure-test.
-build/tool/container-structure-test-linux-amd64 test \
+build/tool/container-structure-test-${cst_ostype}-amd64 test \
     --image corretto-8 \
     --config test-image.yaml
